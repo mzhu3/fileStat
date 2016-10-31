98718dc7afbf9d458290105afa35aaea59b2301d
Matthew Zhu <mzhu3@stuy.edu>
2016-10-31 11:42:48 -0400

    modifying

--- main.c
+++ main.c
@@ -2,12 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-
-int main() {
+char * convertSize(unsigned long size){
+  char *suffix[4] = {"B", "Kb","Mb","Gb"};
+  int suffIndex = 0 ;
+  int end = 0;
+  char ans[256];
+  if(size < 1024){
+    sprintf(ans,"%d",size);
+    strcat(ans,suffix[suffIndex]);
+    return ans;
+  }
+  else{
+    while(size / 1024){
+      suffIndex ++;
+      size = size /1024;
+      end = size;
+    }
+  }
+  sprintf(ans,"%d",end);
+  strcat(ans,suffix[suffIndex]);
+  return ans;
+}
+  int main() {
     struct stat buffer;
     int init = stat("file.txt", &buffer);
     printf("File Size: %lu\n", buffer.st_size);
+    printf("File Size: %s\n", convertSize(buffer.st_size));
     printf("mode: %o\n", buffer.st_mode);
     printf("Last Access: %s\n", ctime(&buffer.st_atime));
     return 0;
